8e3aaf6a60d29aea3cf325cd4df79fcaad19c7f0
Marisa Robayo Moreno <marisarobayo@gmail.com>
2023-04-05 08:12:43 +0200

    Implement logger.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 
+import logger, { type LoggerOptions } from 'pino'
+
+
+let cnf: LoggerOptions = {}
+if (!process.env.VERSION) {
+  cnf.transport = {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      singleLine: true,
+    },
+  }
+}
+export let log = logger(cnf)
